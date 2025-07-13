f5e79a80ddd3ac139883ef4d8d817facfee5c7be
kvndlgs <poissedonn@gmail.com>
2025-07-13 11:55:06 +0000

    lol

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-export const getAllProjects = () => {
-
-}
-
-export const getProjectBySlug = () => {
-
-}
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    },
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
-})
+});
